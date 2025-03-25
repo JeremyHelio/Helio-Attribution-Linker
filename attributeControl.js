525b38ef297da713ab8304a6760acec8567b8fd5
JeremyHelio <jeremy.neilson@madebyhelio.com>
2025-03-25 08:48:50 -0400

    Update attributeControl.js for Helio

--- attributeControl.js
+++ attributeControl.js
@@ -1,20 +1,20 @@
-// Send GET request to advertience API, then push returned data to dataLayer
+// Send GET request to Helio API, then push returned data to dataLayer
 window.attributeControl = (clientTab) => {
   const xhttp = new XMLHttpRequest();
 
   xhttp.onload = () => {
     const data = JSON.parse(xhttp.responseText);
-    data.event = 'adv_pageview';
+    data.event = 'helio_pageview';
   
     window.dataLayer = window.dataLayer || [];
     dataLayer.push(data);
   };
 
   xhttp.onerror = () => {
-    console.log('Error while sending GET request to Advertience API');
+    console.log('Error while sending GET request to Helio API');
 
     window.dataLayer = window.dataLayer || [];
-    dataLayer.push({event: 'adv_pageview'});
+    dataLayer.push({event: 'helio_pageview'});
   };
   
   xhttp.open('GET', 'https://advertience-api.advertience.com/v1/attribute/' + clientTab + '/' + window.location.search);
